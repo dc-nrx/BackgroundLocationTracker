a06285b5e23cf4f5e32dc647531b978ddc93e23f
Dmitry Chapovsky <dmytro.chapovskyi@nimble.com>
2020-03-28 14:27:11 +0200

    Initial Commit

---
+++ LocationTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationTracker
+//
+//  Created by Dmytro Chapovskyi on 28.03.2020.
+//  Copyright © 2020 Dmytro Chapovskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
