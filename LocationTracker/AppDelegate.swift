e473050787c685e0a4bb64e68401ec7531163bb9
Dmitry Chapovsky <dmytro.chapovskyi@nimble.com>
2020-04-21 22:42:37 +0300

    Logging (commented) / actionMinimumInterval removed

--- LocationTracker/AppDelegate.swift
+++ LocationTracker/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		let urlString = "https://www.mocky.io/v2/5185415ba171ea3a00704eed"
 		
-//		BackgroundLocationTracker.shared.start(actionMinimumInterval: 2 * 60, url: NSURL(string: urlString)!, httpHeaders: ["foo": "bar"])
+		BackgroundLocationTracker.shared.start(url: NSURL(string: urlString)!, httpHeaders: ["foo": "bar"])
 		BackgroundLocationTracker.shared.continueIfAppropriate()
 		
 		return true
@@ -43,7 +43,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 	}
-
 
 }
 
